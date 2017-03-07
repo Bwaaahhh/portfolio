3988c12b06fb6f60383ab8061c0701f156f02c09
Unknown <cyril-ix@live.fr>
2017-03-07 16:29:16 +0100

    Update

--- view/index.php
+++ view/index.php
@@ -1,7 +1,22 @@
 <div class="row">
     <div class="text_accroche medium-centered columns">
-        <p>Hey ! Moi c'est cyril Etiévant.</p>
+        <p>Hey ! Moi c'est Cyril Etiévant.</p>
         <p>App & Web développeur indépendant</p>
         <p>près de Besancon en Franche-Comté.</p>
+    </div>
+    <div class="projet">
+        <img src="view/images/projet1.jpg" alt="">
+        <div class="focus_projet">
+            <p>2017</p>
+            <p>APP PROJET</p>
+            <p>#</p>
+            <button type="button" name="button">decouvrir ce projet</button>
+        </div>
+    </div>
+    <div class="projet">
+        <img src="view/images/projet2.jpg" alt="">
+    </div>
+    <div class="projet">
+        <img src="view/images/projet3.jpg" alt="">
     </div>
 </div>
--- view/template/index.php
+++ view/template/index.php
@@ -4,36 +4,37 @@
         <meta charset="utf-8">
         <title>Portfolio Cyril Etievant</title>
         <link rel="stylesheet" type="text/css" href="view/template/foundation.min.css"/>
-        <link rel="stylesheet" type="text/css" href="view/css/style.css">
+        <link rel="stylesheet" href="view/template/font-awesome/css/font-awesome.min.css" />
+        <link rel="stylesheet" type="text/css" href="view/css/style.css" />
         <link rel="stylesheet" type="text/css" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js">
         <link rel="stylesheet" type="text/javascript" src="view/js/js.js">
     </head>
     <body>
         <header>
-            <!-- <div class="menu">
-                <div class="close_menu">
-                    <img src="view/images/exit.png" alt="">
-                    <p>fermer</p>
-                </div>
-                <div class="visu_menu">
-                    <div><p>A PROPOS</p></div>
-                    <div><p>PROJETS</p></div>
-                    <div><p>CONTACT</p></div>
-                </div>
-            </div> -->
-            <div class="menu_visible ">
-                <div class="row">
-                    <div class="link_menu medium-2 columns">
-                        <a href="#" >
-                            <img src="view/images/menu.png" alt="Icone représentant un menu">
-                            <p>MENU</p>
-                        </a>
+            <div class="row fullWidth">
+                <div class="menu large-12 columns">
+                    <div class="close_menu large-4">
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                        <p>fermer</p>
                     </div>
-                    <img class="medium-2 columns medium-offset-3 " src="view/images/logo.png" alt="Un C blanc dans un carré noir. Logo du site">
-                    <div class="renseignements medium-2 medium-offset-3 columns end">
-                        <p>07.00.00.00.00</p>
-                        <p>lalalala@gmail.com</p>
+                    <div class="visu_menu">
+                        <div class="large-4 columns"><a href="index.php?page=apropos"><p>A PROPOS</p></a></div>
+                        <div class="large-4 columns"><a href="index.php?page=projets"><p>PROJETS</p></a></div>
+                        <div class="large-4 columns end"><a href="index.php?page=contact"><p>CONTACT</p></a></div>
                     </div>
+                </div>
+            </div>
+            <div class="row menu_visible ">
+                <div class="link_menu medium-2 columns">
+                    <a href="#" >
+                        <i class="fa fa-bars" aria-hidden="true"></i>
+                        <p>MENU</p>
+                    </a>
+                </div>
+                <img class="medium-1 columns medium-offset-3 " src="view/images/logo.png" alt="Un C blanc dans un carré noir. Logo du site">
+                <div class="renseignements medium-2 medium-offset-3 columns end">
+                    <p>07.00.00.00.00</p>
+                    <p>lalalala@gmail.com</p>
                 </div>
             </div>
         </header>
@@ -43,9 +44,9 @@
 
         <footer>
             <div class="row">
-                <div class="medium-5 columns centered">
+                <div class="medium-5 columns large-centered ">
                     <img src="view/images/copyright.svg" alt="">
-                    <p>cyril étiévant - tous droits réservés.</p>
+                    <p>cyril etievant - tous droits reserves.</p>
                 </div>
             </div>
         </footer>
