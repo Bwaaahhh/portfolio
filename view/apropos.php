902d4e3e3b001a1ddc65d480c22becb08d48cff4
Unknown <cyril-ix@live.fr>
2017-03-14 09:56:26 +0100

    Add + Update AboutMe Page

---
+++ view/apropos.php
@@ -0,0 +1,27 @@
+<div class="apropos">
+    <div class="row">
+        <div class="large-12 columns premier">
+            <p>Hey! Moi c'est Cyril Etievant,</p>
+            <p>App & Web développeur indépendant</p>
+            <p>près de besancon, en Franche-Comté.</p>
+        </div>
+        <div class="second">
+            <p>Présentement, je parfais ma formation</p>
+            <p>dans le digital avec <a href="http://www.accesscodeschool.fr/"> Acces Code School</a></p>
+            <p>puis je vais effectuer un stage de plusieurs</p>
+            <p>semaines à l'agence <a href="http://www.spin-on.fr/">SpinOn</a>  à Besançon</p>
+        </div>
+        <div class="troisieme">
+            <p>Si vous souhaitez collaborer avec moi pour un</p>
+            <p>futur projet, n'hésitez pas à <a href="index.php?page=contact.php">me contacter</a>  :)</p>
+        </div>
+        <div class="quatrieme">
+            <p>//</p>
+        </div>
+        <div class="divButton">
+            <a href="data/CV.pdf" download="CV Etievant Cyril" class="large-3 large-offset-1 columns">telecharger mon cv (.pdf)</a>
+            <a href="https://www.linkedin.com/in/cyril-etievant-bbb540137/" class="large-3 large-offset-1 columns">consulter mon profil linkedin</a>
+            <a href="https://github.com/Bwaaahhh" class="large-3 large-offset-1 columns">accéder à mon github</a>
+        </div>
+    </div>
+</div>
