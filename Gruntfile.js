3d8a922215fcc60ecd07a7b6f62e262444065143
Unknown <cyril-ix@live.fr>
2017-03-16 14:13:52 +0100

    Update Gruntfile Add comment for next time

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,26 @@
+// Ligne a taper pour que ce fichier Gruntfile.js marche correctement
+
+//  npm init puis plusieurs fois entrée
+//  npm install -g grunt-cli
+//  npm install grunt --save-dev
+//  touch Gruntfile.js
+//  npm install --save-dev load-grunt-tasks
+//  require('load-grunt-tasks')(grunt);
+//
+//  npm install grunt-contrib-watch --save-dev
+//  npm install grunt-contrib-sass --save-dev
+//  npm install grunt-ftp-deploy --save-dev
+//     touch .ftppass pour mettre les logs du serveur comme ceci
+                    //     {
+                    //   "key1": {
+                    //     "username": "username",
+                    //     "password": "password"
+                    //   }
+                    // }
+//
+//
+
+
 module.exports = function(grunt) {
 
     require('load-grunt-tasks')(grunt);
@@ -17,30 +40,28 @@ module.exports = function(grunt) {
         'ftp-deploy': {
           build: {
             auth: {
-                host: 'cyrile.marmier.codeur.online',
-                port: 21,
-                authKey: 'key1'
+                host: 'cyrile.marmier.codeur.online',   // host du serveur
+                port: 21,                               // port
+                authKey: 'key1'                         // log relié au fichier .ftppass
             },
-                src: 'view/css',
-                dest: '/home/cyrile/portfolio-perso/view/css',
+                src: 'view/css',                        // dossier source
+                dest: '/home/cyrile/portfolio-perso/view/css',   // dossier cible
             //    exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db', 'path/to/dist/tmp']
             }
         },
 
         watch:{
             dist:{
-                files : ['view/scss/*.scss'],
-                tasks: ['sass:dist' , 'ftp-deploy'],
+                files : ['view/scss/*.scss'],            // dossier que le watch regarde
+                tasks: ['sass:dist' , 'ftp-deploy'],                    // tache qu'il fait a la modification
                 options:{
-                    spawn: false,
-                    livereload: true,
-                }
-            }
+                    spawn: false,                        // gain de temps
+                    livereload: true,                    // livereload a la modification du scss  port utilisé de base 35729
+                }     // <script src="//localhost:35729/livereload.js"></script>
+            }         //  A placer juste avant la fermeture de la balise </body>  pour que le livereload fonctionne
         }
     });
 
-  // Import du package
-  //grunt.loadNpmTasks('grunt-contrib-sass')
 
   // Redéfinition de la tâche `default` qui est la tâche lancée dès que vous lancez Grunt sans rien spécifier.
   // Note : ici, nous définissons sass comme une tâche à lancer si on lance la tâche `default`.
